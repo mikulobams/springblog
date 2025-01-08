7c2c679d4cb80a96056ae562b93da8d8c3f307b2
Michael Bamikunle <mikulobams@gmail.com>
2025-01-08 23:21:15 +0000

    Added post repository and removed hardcording

--- src/main/java/com/apress/demo/springblog/controller/PostController.java
+++ src/main/java/com/apress/demo/springblog/controller/PostController.java
@@ -1,27 +1,38 @@
 package com.apress.demo.springblog.controller;
 
 import com.apress.demo.springblog.domain.Post;
+import com.apress.demo.springblog.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
 
 @Controller
 @RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
+
     @GetMapping
     public String postPage(Model model) {
-        Post post = new Post();
-        post.setTitle("Hello Spring Boot");
-        post.setDescription("Spring Boot");
-        post.setBody("Spring Boot is Awesome");
-        Post post1 = new Post();
-        post1.setTitle("Hello Spring Boot 3");
-        post1.setDescription("Spring Boot 3");
-        post1.setBody("Spring Boot 3 is Awesome");
-        model.addAttribute("posts", Arrays.asList(post, post1));
+        model.addAttribute("posts", postService.findAllPosts());
         return "post";
+    }
+
+    @GetMapping("/add")
+    public String addPostPage(Model model) {
+        model.addAttribute("post", new Post());
+        return "addPost";
+    }
+
+    @PostMapping
+    public String addPost(@ModelAttribute("post") Post post) {
+        postService.addPost(post);
+        return "redirect:/posts";
     }
 }
---
+++ src/main/java/com/apress/demo/springblog/service/PostService.java
@@ -0,0 +1,22 @@
+package com.apress.demo.springblog.service;
+
+import com.apress.demo.springblog.domain.Post;
+import com.apress.demo.springblog.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+
+    public void addPost(Post post) {
+        postRepository.addPost(post);
+    }
+
+    public Set<Post> findAllPosts() {
+        return postRepository.findAllPosts();
+    }
+}
